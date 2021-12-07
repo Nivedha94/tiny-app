8caeff42f4d1948293f40c42836a833b5e51423c
vagrant <nivedha.selvan@gmail.com>
2021-12-07 08:58:50 +0000

    HTML code rendered to the client browser

--- express_server.js
+++ express_server.js
@@ -18,6 +18,11 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//HTML code rendered to the client browser
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
